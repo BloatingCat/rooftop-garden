3cf51ed6b95f30c5b09c7024abdfa3fb4d2b254f
BloatingCat <nguyenvokhanhvan0@gmail.com>
2026-05-20 09:40:17 +0700

    Added ServingSystem

---
+++ systems/serving_system.gd
@@ -0,0 +1,46 @@
+class_name ServingSystem
+extends Node
+
+const BASE_SERVE_TIME := 3.0
+
+var _serving: bool = false
+var _serve_accum: float = 0.0
+var _current_customer: Dictionary = {}
+
+func _process(delta: float) -> void:
+	if not _serving:
+		_try_start_serving()
+		return
+	_serve_accum += delta
+	if _serve_accum >= _serve_time():
+		_complete_serve()
+
+func _try_start_serving() -> void:
+	if GameState.food < 1:
+		return
+	if GameState.customers.is_empty():
+		return
+	_current_customer = GameState.customers[0]
+	_serving = true
+	_serve_accum = 0.0
+	Events.serving_started.emit(_current_customer)
+
+func _complete_serve() -> void:
+	# customer may have left while being served
+	if not GameState.customers.has(_current_customer):
+		_reset()
+		return
+	GameState.customers.erase(_current_customer)
+	GameState.food -= 1
+	GameState.coins += 5
+	Events.customer_served.emit(_current_customer)
+	_reset()
+
+func _reset() -> void:
+	_serving = false
+	_serve_accum = 0.0
+	_current_customer = {}
+
+func _serve_time() -> float:
+	return BASE_SERVE_TIME
+	# later: return BASE_SERVE_TIME * GameState.serve_speed_multiplier
